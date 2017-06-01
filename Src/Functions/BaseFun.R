f341cff8ff93e9d5b96d8ccac5e275acb1b91a9f
arekbee <arekbee@gmail.com>
2017-06-01 18:51:27 +0200

    Update BaseFun.R

--- Src/Functions/BaseFun.R
+++ Src/Functions/BaseFun.R
@@ -1,32 +1,33 @@
-getStooq <- function(asset) {
-  as.url <- paste0("http://stooq.com/q/d/l/?s=", asset, "&i=d")
-  as.dir <- file.path(".","tempData")
-  if(!dir.exists(as.dir))
-  {
-    dir.create(as.dir)
-  }
-  as.fileName <- paste0(asset, ".csv")
-  as.file<- file.path(as.dir,as.fileName)
-  
-  if(file.exists(as.file) & file.size(as.file) >0) 
-  {
+getStooq <- function(asset, useReadCsv=T) {  
+as.url <- paste0("http://stooq.com/q/d/l/?s=", asset, "&i=d")
+    as.dir <- file.path(".","tempData")
+    if(!dir.exists(as.dir))    
+    {      
+    dir.create(as.dir)   
+    }    
+    as.fileName <- paste0(asset, ".csv")
+    as.file<- file.path(as.dir,as.fileName)
+    if(file.exists(as.file) & file.size(as.file) >0)     {
     sprintf("file exist %s", as.file)
     ret <- read.csv(file=as.file, strip.white = T)
-    ret$Date <- as.Date(ret$Date)
-  }else
-  {
+    ret$Date <- as.Date(ret$Date)      
+    }else {        
+    if(useReadCsv) {          
+    ret <- read.csv(as.url)  
+    ret$Date <- as.Date(ret$Date)        
+    } else {          
     u <- getURI(as.url, ssl.verifypeer=0L, followlocation=1L)
     ret <- read.csv(text=u)
-    ret$Date <- as.Date(ret$Date)
+    ret$Date <- as.Date(ret$Date)        
+    }
     write.csv(ret, file = as.file)
-  }
-  #download.file(url=as.url, destfile = as.file)
-
-  #ret$Month <- format(ret$Date, "%Y-%m")
-  
-  
-  return (ret)
-}
+    }    
+    #download.file(url=as.url, destfile = as.file)    
+    #ret$Month <- format(ret$Date, "%Y-%m")
+    ret$Month <- format(ret$Date, "%Y-%m")
+    ret$Date.POSIXct  <- as.POSIXct(ret$Date)  
+    return (ret)
+   }
 
 addNRow <- function(df) {
   df$NRow <- 1:nrow(df);
